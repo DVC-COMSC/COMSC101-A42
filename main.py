a6ffa6d9a9b0178828ebf988572bd7a50b2e5778
Kyu Lee <kwooler@gmail.com>
2023-03-10 23:07:43 -0800

    A42

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def main():
+    numbers = [[11, 12, 13, 14, 15],
+               [21, 22, 23, 24, 25],
+               [31, 32, 33, 34, 35]]
+
+    rownum = int(input('Enter the row num: '))
     ##################################################
     # Comlete your code here
     ##################################################
-    number = int(input('Enter the number: '))
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -7,7 +7,7 @@ import re
 def test_main_1():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '10'
+    datastr = '2'
     sys.stdin = io.StringIO(datastr)
 
     main.main()
@@ -18,7 +18,7 @@ def test_main_1():
 
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*[f,F]alse[\w,\W]*'
+    regex_string = r'[\w,\W]*165[\w,\W]*'
     res = re.search(regex_string, lines[0])
     assert res != None
     print(res.group())
@@ -27,7 +27,7 @@ def test_main_1():
 def test_main_2():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '13'
+    datastr = '1'
     sys.stdin = io.StringIO(datastr)
 
     main.main()
@@ -38,27 +38,7 @@ def test_main_2():
 
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*[t,T]rue[\w,\W]*'
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
-
-
-def test_main_3():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-    datastr = '23'
-    sys.stdin = io.StringIO(datastr)
-
-    main.main()
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
-
-    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
-    # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*[t,T]rue[\w,\W]*'
+    regex_string = r'[\w,\W]*115[\w,\W]*'
     res = re.search(regex_string, lines[0])
     assert res != None
     print(res.group())
